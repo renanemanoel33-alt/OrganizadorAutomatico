bcf74108e32d58e6805b6ff3395520231aadcfb2
Renan Emanoel <renanemanoel33@gmail.com>
2026-03-16 17:10:14 -0300

    Create Organizador.py

---
+++ Organizador.py
@@ -0,0 +1,20 @@
+import os
+from tkinter.filedialog import askdirectory
+def organizar(organizador):
+    pain=askdirectory(title='Selecione a pasta onde estão os arquivos')
+    list=os.listdir(pain)
+    local={
+        'imagens':['.png','.jpg','.jpeg'],
+        'documentos':['.pdf','.doc','.docx'],
+        'musicas':['.mp3','.wav'],
+        'videos':['.mp4','.avi'],
+        'setup':['.exe','.msi'],
+    }
+    for arquivo in list:
+        nome, extensao = os.path.splitext(arquivo)
+        for pasta in local:
+            if extensao in local[pasta]:
+                if not os.path.exists(f'{pain}/{pasta}'):
+                    os.mkdir(f'{pain}/{pasta}')
+                os.rename(f'{pain}/{arquivo}',f'{pain}/{pasta}/{arquivo}')
+organizar('organizador')
